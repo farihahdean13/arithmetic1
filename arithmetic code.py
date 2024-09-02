4a874b66da024df2d990178bc8810fa9edd0bd56
farihahdean13 <farihahdean13@gmail.com>
2024-09-02 23:42:52 +0800

    Add files via upload

--- arithmetic code.py
+++ arithmetic code.py
@@ -11,3 +11,12 @@ multiplication = a*b
 print("multiplication =", multiplication)
 division = a/b
 print("division =", division)
+
+c=1
+print(c)
+float("1.25")
+print(float("1.25"))
+
+float = 1.25
+print('The result of a divided by b is:', type(float))
+
