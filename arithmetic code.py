5d5ecb64ec688c79feaf759763005f7c53c48baf
farihahdean13 <farihahdean13@gmail.com>
2024-09-02 22:57:20 +0800

    Add files via upload

--- arithmetic code.py
+++ arithmetic code.py
@@ -2,3 +2,12 @@ a=15
 b=12
 print(a)
 print(b)
+
+sum = a+b
+print("addition =", sum)
+subtraction = a-b
+print("subtraction =", subtraction)
+multiplication = a*b
+print("multiplication =", multiplication)
+division = a/b
+print("division =", division)
